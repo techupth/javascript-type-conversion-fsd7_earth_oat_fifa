cff7452fae2d6c3971f06e2bb5716d3e2c28a232
Oat Chana <chanathip0944729842@gmail.com>
2024-09-30 14:11:35 +0700

    fix:type conversion debug

--- ex-1.js
+++ ex-1.js
@@ -1,7 +1,8 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
 
-accountBalance = accountBalance + depositAmountFromUser;
+accountBalance = accountBalance + Number(depositAmountFromUser);
+accountBalance = String(accountBalance)
 
 let accountBalanceMessage = "Your account balance is " + accountBalance;
 
